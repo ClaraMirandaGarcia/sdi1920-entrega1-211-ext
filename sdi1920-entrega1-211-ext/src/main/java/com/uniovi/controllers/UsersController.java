3ae445c125d77377729083e94642143ac22b0f14
Clara <UO264958@uniovi.es>
2020-06-03 16:09:53 +0200

    Feat 2&3

--- sdi1920-entrega1-211-ext/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi1920-entrega1-211-ext/src/main/java/com/uniovi/controllers/UsersController.java
@@ -51,5 +51,15 @@ public class UsersController {
 		securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
 		return "redirect:home";
 	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model, String error, String logout) {
+		if (error != null)
+			model.addAttribute("error", "Your username and password is invalid.");
+		if (logout != null)
+			model.addAttribute("message", "You have been logged out successfully.");
+
+		return "login";
+	}
 
 }
