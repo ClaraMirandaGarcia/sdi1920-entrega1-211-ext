8cb52cbf132c1a1c075c3d4c1fc81223e5bdc97a
Clara <UO264958@uniovi.es>
2020-06-06 19:13:00 +0200

    F18

--- sdi1920-entrega1-211-ext/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi1920-entrega1-211-ext/src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.uniovi.controllers;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.uniovi.entities.User;
 import com.uniovi.services.RolesService;
@@ -37,41 +39,55 @@ public class UsersController {
 	@Autowired
 	private SignUpFormValidator signUpFormValidator;
 
+	@RequestMapping(value = "/user/delete", method = RequestMethod.POST)
+	public String delete(RedirectAttributes rattrs,
+			@RequestParam(value = "userDelete", required = false) List<String> emails) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String userEmail = auth.getName();
+		if (!emails.contains(userEmail)) {
+			for (String email : emails) {
+				usersService.deleteUser(usersService.getUserByEmail(email).getId());
+			}
+		} else {
+			rattrs.addFlashAttribute("error", "You can not delete your own account");
+		}
+		return "redirect:list";
+	}
+
 	@RequestMapping("/user/list")
-	public String getListado(Model model, Pageable pageable, @RequestParam(value = "", required = false) String searchText) {
-		
+	public String getListado(Model model, Pageable pageable,
+			@RequestParam(value = "", required = false) String searchText) {
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		String email = auth.getName();
 		User user = usersService.getUserByEmail(email);
-		
+
 		Page<User> users = new PageImpl<User>(new LinkedList<User>());
 		users = usersService.getUsersFor(pageable, email);
-		
-		//ROLE_USER
-		if(user.getRole().equals(rolesService.getRoles()[1])) {
+
+		// ROLE_USER
+		if (user.getRole().equals(rolesService.getRoles()[1])) {
 			if (searchText != null && !searchText.isEmpty()) {
-	            users = usersService.searchByNameSurnameOrEmail(pageable, searchText, email);
-	        } else {
-	            users = usersService.getUsersFor(pageable, email);
-	        }
-		} else if(user.getRole().equals(rolesService.getRoles()[0])) {
+				users = usersService.searchByNameSurnameOrEmail(pageable, searchText, email);
+			} else {
+				users = usersService.getUsersFor(pageable, email);
+			}
+		} else if (user.getRole().equals(rolesService.getRoles()[0])) {
 			if (searchText != null && !searchText.isEmpty()) {
-	            users = usersService.searchByNameSurnameOrEmailAdmin(pageable, searchText, email);
-	        } else {
-	            users = new PageImpl<User>(usersService.getUsers());
-	        }
+				users = usersService.searchByNameSurnameOrEmailAdmin(pageable, searchText, email);
+			} else {
+				users = new PageImpl<User>(usersService.getUsers());
+			}
 		}
-		
-		
-		//Check if already an invitation
-		//Check if already a friend
-		
+
+		// Check if already an invitation
+		// Check if already a friend
+
 		model.addAttribute("usersList", users.getContent());
 		model.addAttribute("page", users);
-		
+
 		return "user/list";
 	}
-	
 
 	// Basic methods of the application
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
